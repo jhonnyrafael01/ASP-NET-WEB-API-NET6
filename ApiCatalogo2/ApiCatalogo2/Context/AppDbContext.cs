6fe839da2b49b34d0dcf84885cfa2f1a40533e2d
Jhonny Rafael <rafaeljhonny01@gmail.com>
2023-03-21 14:07:10 -0300

    Criando o arquivo de contexto

---
+++ ApiCatalogo2/ApiCatalogo2/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiCatalogo2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo2.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }   
+    }
+}
